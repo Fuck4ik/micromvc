6447e608a6878dd9f1af6f4175f77df7943a3342
SkillUP Student <student@skillup.spb.ru>
2018-03-15 22:07:44 +0300

    Add scripts

--- templates/views/home.php
+++ templates/views/home.php
@@ -12,3 +12,5 @@
     <a href="<?= $this->route('reg') ?>" class="btn">Регистрация</a>
     <a href="<?= $this->route('weather') ?>" class="btn">Погода</a>
 </div>
+
+<script src="templates/assets/js/script.js?<?= filemtime('templates/assets/js/script.js') ?>"></script>
