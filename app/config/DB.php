09cd89933313b2aab4b6d8bcb59c4a28e3cd108b
SkillUP Student <student@skillup.spb.ru>
2018-03-20 19:26:24 +0300

    Refresh posts sql and db config

--- app/config/DB.php
+++ app/config/DB.php
@@ -7,10 +7,10 @@ class DB
     public static $connections = [
         'default' => [
             'driver' => 'mysql',
-            'db' => 'omasn',
+            'db' => 'php2',
             'host' => 'localhost',
-            'username' => 'omasn',
-            'password' => 'admin1q2',
+            'username' => 'root',
+            'password' => 'root',
             'charset' => 'utf8',
             'errors' => true,
         ],
